da567f963a840ce8fe19a5291a8a58f475797c0f
christiansuar <160215893+christiansuar@users.noreply.github.com>
2024-11-11 02:49:07 -0600

    Added some UI

--- Scenes/Cutscenes/intro.gd
+++ Scenes/Cutscenes/intro.gd
@@ -2,5 +2,16 @@ extends Node3D
 
 @onready var intro_player = $AnimationPlayer
 
+
 func _ready() -> void:
+	intro_player.play("Idle (main menu)")
+
+
+func _on_button_pressed() -> void:
+	intro_player.stop()
+	
 	intro_player.play("Mission Brief")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().quit()
