0b6f9be9e1e5613069bbfb29851db9764edc5a38
christiansuar <160215893+christiansuar@users.noreply.github.com>
2024-11-12 02:13:30 -0600

    Main Menu and Idle screen fade
    add intro dialogue

--- Scenes/Cutscenes/intro.gd
+++ Scenes/Cutscenes/intro.gd
@@ -1,8 +1,8 @@
 extends Node3D
 
 @onready var intro_player = $AnimationPlayer
-@onready var tween = create_tween()
-@onready var env_exposure = $WorldEnvironment.environment.tonemap_exposure
+@onready var spam_prev = true
+@onready var env_exposure = $WorldEnvironment.environment
 
 
 func _ready() -> void:
@@ -10,9 +10,20 @@ func _ready() -> void:
 
 
 func _on_button_pressed() -> void:
-	intro_player.stop()
-	tween.tween_property($mainmenu, "modulate.a", 0.00, 5.0)
-	intro_player.play("Mission Brief")
+	if spam_prev:
+		#PREVENTS SPAMMING THE BUTTON
+		spam_prev = false
+		
+		var tween1 = get_tree().create_tween()
+		var tween2 = get_tree().create_tween()
+		
+		tween1.tween_property(env_exposure, "tonemap_exposure", 0.0, 5.0)
+		tween2.tween_property($mainmenu, "modulate:a", 0.0, 5.0)
+		
+		await tween1.finished
+		
+		
+		intro_player.play("Mission Brief")
 
 
 func _on_button_2_pressed() -> void:
