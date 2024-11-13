2cfc533965299cfd148f71eaa55886d26159deae
christian-suar <148103464+christian-suar@users.noreply.github.com>
2024-11-13 08:54:18 -0600

    added rocket fuel thing

--- Scenes/Cutscenes/intro.gd
+++ Scenes/Cutscenes/intro.gd
@@ -9,22 +9,29 @@ func _ready() -> void:
 	intro_player.play("Idle (main menu)")
 
 
-func _on_button_pressed() -> void:
+
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_start_pressed() -> void:
 	if spam_prev:
 		#PREVENTS SPAMMING THE BUTTON
 		spam_prev = false
 		
 		var tween1 = get_tree().create_tween()
 		var tween2 = get_tree().create_tween()
+		var tween3 = get_tree().create_tween()
+		var tween4 = get_tree().create_tween()
 		
 		tween1.tween_property(env_exposure, "tonemap_exposure", 0.0, 5.0)
 		tween2.tween_property($mainmenu, "modulate:a", 0.0, 5.0)
+		tween3.tween_property($AnimationPlayer/SpaceShip, "position:z", -100, 5.0)
+		tween4.tween_property($AnimationPlayer/Camera3D, "rotation:y" , -45, .5).as_relative()
 		
 		await tween1.finished
 		
 		
 		intro_player.play("Mission Brief")
-
-
-func _on_button_2_pressed() -> void:
-	get_tree().quit()
