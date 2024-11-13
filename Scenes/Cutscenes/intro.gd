152a1f73d603855199917d2f6028e93543c3078a
christian-suar <148103464+christian-suar@users.noreply.github.com>
2024-11-13 11:06:14 -0600

    Trying to fix camera for intro

--- Scenes/Cutscenes/intro.gd
+++ Scenes/Cutscenes/intro.gd
@@ -29,7 +29,7 @@ func _on_start_pressed() -> void:
 		tween1.tween_property(env_exposure, "tonemap_exposure", 0.0, 5.0)
 		tween2.tween_property($mainmenu, "modulate:a", 0.0, 5.0)
 		tween3.tween_property($AnimationPlayer/SpaceShip, "position:z", -100, 5.0)
-		tween4.tween_property($AnimationPlayer/Camera3D, "rotation:y" , -45, .5).as_relative()
+		tween4.tween_property($AnimationPlayer/Camera3D, "rotation:y" , 46.2, .5).as_relative()
 		
 		await tween1.finished
 		
